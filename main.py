d53c18133da60bb08b432c919bce6412a7e8e6d4
DonKongChys <doancongtrics@gmail.com>
2024-08-03 09:56:00 +0700

    pussh lai de cai lai win

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import asyncio
+import random
 from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
-from openai import OpenAI
+# from openai import OpenAI
 import json
 import requests
+from kafka import KafkaProducer
+from datetime import time
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.common.proxy import *
+# import undetected_chromedriver as uc
+import seleniumwire.undetected_chromedriver as uc
+
 
 # import requests
 
 SBR_WS_CDP = 'wss://brd-customer-hl_dc842eb8-zone-real_estate_browser:zfd36w1amt2d@brd.superproxy.io:9222'
 BASE_URL = "https://zoopla.co.uk"
 LOCATION = "London"
-API_KEY =  "AIzaSyD7_v-68hNwRucbiUnhQ6OdRKu66wrMbys"
-
-
-client = OpenAI(
-    api_key="sk-proj-WDkFvM4XYnDpQXmHNwpGT3BlbkFJcmCCKMu8sUfeiXFRwFfQ")
+API_KEY = "AIzaSyD7_v-68hNwRucbiUnhQ6OdRKu66wrMbys"
 
 
 def extract_picture(picture_sections):
@@ -27,42 +35,6 @@ def extract_picture(picture_sections):
             if source_type == 'webp' and '1024' in pic_url:
                 picture_sources.append(pic_url)
     return picture_sources
-
-
-# def extract_property_details(input):
-#     print("Extracting property details....")
-#     command = """
-#         You are a data extractor model and you have been tasked with extracting information about the apartment for me into json
-#         Here is the div for the property details:
-        
-#         {input_command}
-#         {{
-#             "price": "",
-#             "address": "",
-#             "bedrooms": "",
-#             "bathrooms": "",
-#             "receptions": "",
-#             "EPC Rating": "",
-#             "tenure": "",
-#             "time_remaining_on_lease": ""
-#             "service_charge":""
-#             "countil_tax_band": "",
-#             "ground_rent": ""
-#         }}
-#     """.format(input_command = input)
-    
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages= [
-#             {
-#                 "role": "user",
-#                 "content": command
-#             }
-#         ]
-#     )
-#     res = response.choices[0].message.content
-#     json_data = json.loads(res)
-#     return json_data
 
 
 def extract_property_details(input):
@@ -88,7 +60,7 @@ def extract_property_details(input):
             "ground_rent": ""
         }}
     """
-    
+
     url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + API_KEY
     headers = {"Content-Type": "application/json"}
     data = {
@@ -101,20 +73,29 @@ def extract_property_details(input):
         ]
     }
 
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    retries = 3
+    backoff_factor = 2
 
-    if response.status_code == 200:
-        res = response.json()
-        # extracted_data = json.loads(res['candidates'][0]['content'])
-        res = res['candidates'][0]['content']
-        json_string = res['parts'][0]['text']
-        json_string = json_string.replace('```json\n', '').replace('\n```', '')
-        extracted_data = json.loads(json_string)
-        return extracted_data
-    else:
-        print(f"Error: {response.status_code}")
-        print(response.text)
-        return None
+    for attempt in range(retries):
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        if response.status_code == 200:
+            res = response.json()
+            res = res['candidates'][0]['content']
+            json_string = res['parts'][0]['text']
+            json_string = json_string.replace(
+                '```json\n', '').replace('\n```', '')
+            extracted_data = json.loads(json_string)
+            return extracted_data
+        elif response.status_code == 503 and attempt < retries - 1:
+            sleep_time = backoff_factor ** attempt
+            print(f"Error: {response.status_code}. Retrying in {sleep_time} seconds...")
+            time.sleep(sleep_time)
+        else:
+            print(f"Error: {response.status_code}")
+            print(response.text)
+            return None
+
 
 def extract_floor_plan(soup):
     print("Extracting floor plan....")
@@ -124,26 +105,146 @@ def extract_floor_plan(soup):
         floor_plan_src = floor_plan.find("picture").find("source")['srcset']
         plan["floor plan"] = floor_plan_src.split(' ')[0]
     return plan
-    
-    
-    
-    
-async def run(pw):
-    print('Connecting to Scraping Browser...')
-    browser = await pw.chromium.connect_over_cdp(SBR_WS_CDP)
+
+
+# async def run(pw, producer):
+#     print('Connecting to Scraping Browser...')
+#     # browser = await pw.chromium.connect_over_cdp(SBR_WS_CDP)
+#     # browser = await pw.webkit.launch()
+#     # browser = await pw.chromium.launch()
+#     # browser = await pw.firefox.launch()
+#     browser =await pw.chromium.launch(headless=True)  # Chạy ở chế độ nền
+#     context =await browser.new_context(
+#         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+#         accept_downloads=True,
+#         javascript_enabled=False,
+#         # ignore_https_errors=True,
+#         # bypass_csp=True,
+#     )
+
+#     try:
+#         # page = await browser.new_page()
+#         page =await context.new_page()
+
+#         print(f'Connected! Navigating to {BASE_URL}...')
+#         await page.goto(BASE_URL)
+#         await page.wait_for_load_state('load')
+
+#         # enter London to search bar
+#         await page.fill('input[name="autosuggest-input"]', LOCATION, timeout=60000)
+#         await page.keyboard.press("Enter")
+#         print("Waiting for results....")
+#         await page.wait_for_load_state("load")
+#         await page.wait_for_load_state('networkidle')
+
+#         with open(f"test_{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.html", "w", encoding="utf-8") as f:
+#             f.write(await page.content())
+
+#         content = await page.inner_html('div[data-testid="regular-listings"]')
+
+#         soup = BeautifulSoup(content, 'html.parser')
+#         # soup = soup.find("div", {"data-testid": "regular-listings"})
+
+#         items = soup.findAll("div", class_="dkr2t83")
+
+#         for idx, div in enumerate(items):
+
+#             data = {}
+
+#             data.update(
+#                 {'address': div.find("address").text,
+#                  'title': div.find("h2").text,
+#                  'link': BASE_URL+div.find("a")['href']}
+#             )
+
+#             # Get data in detail
+#             print(f"Navigating to the listing page... {data['link']}")
+#             await page.goto(data['link'])
+#             await page.wait_for_load_state("load")
+
+#             content = await page.inner_html('div[data-testid="listing-details-page"]')
+#             soup = BeautifulSoup(content, 'html.parser')
+
+#             picture_section = soup.find(
+#                 'section', {'aria-labelledby': 'listing-gallery-heading'})
+#             pictures = extract_picture(picture_section)
+#             data['pictures'] = pictures
+
+#             # extract detail
+#             property_details = soup.select_one('div[class="_14bi3x331"]')
+#             property_details = extract_property_details(property_details)
+
+#             floor_plan = extract_floor_plan(soup)
+
+#             data.update(property_details)
+#             data.update(floor_plan)
+
+#             print(data)
+#             break
+
+#             # print("Sending data to kafka...")
+#             # producer.send("properties", value=json.dumps(data).encode('utf-8'))
+#             # print("Data sent to kafka!")
+
+#             # break
+
+#         # CAPTCHA handling: If you're expecting a CAPTCHA on the target page, use the following code snippet to check the status of Scraping Browser's automatic CAPTCHA solver
+#         # client = await page.context.new_cdp_session(page)
+#         # print('Waiting captcha to solve...')
+#         # solve_res = await client.send('Captcha.waitForSolve', {
+#         #     'detectTimeout': 10000,
+#         # })
+#         # print('Captcha solve status:', solve_res['status'])
+#         print('Navigated! Scraping page content...')
+#         # html = await page.content()
+#         # print(html)
+#     finally:
+#         await browser.close()
+
+
+async def run(producer):
+
+    ## Set chrome Options
+    chrome_options = uc.ChromeOptions()
+
+    ## Disable loading images for faster crawling
+    options.add_argument('--blink-settings=imagesEnabled=false')
+
+    # Tùy chọn cho Chrome
+    options = Options()
+    # options.add_argument("--headless=new")  # Chạy Chrome ở chế độ nền (headless)
+    # options.add_argument("--enable-javascript")
+
+    # myProxy = "149.215.113.110:70"
+    # proxy = Proxy({
+    #     'proxyType': ProxyType.MANUAL,
+    #     'httpProxy': myProxy,
+    #     'sslProxy': myProxy,
+    #     'noProxy': ''})
+    # options.proxy = proxy
+
+    # Khởi tạo trình duyệt Chrome
+    driver = webdriver.Chrome(options=options)
+
     try:
-        page = await browser.new_page()
-        print(f'Connected! Navigating to {BASE_URL}...')
-        await page.goto(BASE_URL)
+        print(f"Navigating to {BASE_URL}")
+        # Truy cập URL
+        driver.get(
+            "https://www.zoopla.co.uk/for-sale/property/london/?q=London&results_sort=newest_listings&search_source=home")
 
-        # enter London to search bar
-        await page.fill('input[name="autosuggest-input"]', LOCATION)
-        await page.keyboard.press("Enter")
-        print("Waiting for results....")
-        await page.wait_for_load_state("load")
+        # Chờ Cloudflare hoàn thành thử thách (điều chỉnh thời gian chờ nếu cần)
+        driver.implicitly_wait(60)
 
-        content = await page.inner_html('div[data-testid="regular-listings"]')
-        soup = BeautifulSoup(content, 'html.parser')
+        # time.sleep(10)
+
+        # Lấy HTML sau khi vượt qua Cloudflare
+        html = driver.page_source
+
+        with open(f"zoopla_{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.html", "w", encoding="utf-8") as f:
+            f.write(html)
+
+        soup = BeautifulSoup(html, 'html.parser')
+        soup = soup.find("div", {"data-testid": "regular-listings"})
 
         items = soup.findAll("div", class_="dkr2t83")
 
@@ -157,47 +258,46 @@ async def run(pw):
                  'link': BASE_URL+div.find("a")['href']}
             )
 
-            # Get data in detail
-            print(f"Navigating to the listing page... {data['link']}")
-            await page.goto(data['link'])
-            await page.wait_for_load_state("load")
-
-            content = await page.inner_html('div[data-testid="listing-details-page"]')
-            soup = BeautifulSoup(content, 'html.parser')
-
-            picture_section = soup.find(
-                'section', {'aria-labelledby': 'listing-gallery-heading'})
-            pictures = extract_picture(picture_section)
-            data['pictures'] = pictures
-
-            # extract detail
-            property_details = soup.select_one('div[class="_14bi3x331"]')
-            property_details = extract_property_details(property_details)
-
-            floor_plan = extract_floor_plan(soup)
-            
-            data.update(property_details)
-            data.update(floor_plan)
             print(data)
-            break
 
-        # CAPTCHA handling: If you're expecting a CAPTCHA on the target page, use the following code snippet to check the status of Scraping Browser's automatic CAPTCHA solver
-        # client = await page.context.new_cdp_session(page)
-        # print('Waiting captcha to solve...')
-        # solve_res = await client.send('Captcha.waitForSolve', {
-        #     'detectTimeout': 10000,
-        # })
-        # print('Captcha solve status:', solve_res['status'])
+            # # Get data in detail
+            # print(f"Navigating to the listing page... {data['link']}")
+            # driver.get(data['link'])
+            # # await page.wait_for_load_state("load")
+            # driver.implicitly_wait(30)
+
+            # content = driver.find_element(By.CSS_SELECTOR, "div[data-testid='listing-details-page']")
+            # soup = BeautifulSoup(content, 'html.parser')
+
+            # picture_section = soup.find(
+            #     'section', {'aria-labelledby': 'listing-gallery-heading'})
+            # pictures = extract_picture(picture_section)
+            # data['pictures'] = pictures
+
+            # # extract detail
+            # property_details = soup.select_one('div[class="_14bi3x331"]')
+            # property_details = extract_property_details(property_details)
+
+            # floor_plan = extract_floor_plan(soup)
+
+            # data.update(property_details)
+            # data.update(floor_plan)
+
+            # print(data)
+            # break
+
         print('Navigated! Scraping page content...')
-        # html = await page.content()
-        # print(html)
+
     finally:
-        await browser.close()
+        driver.quit()
 
 
 async def main():
-    async with async_playwright() as playwright:
-        await run(playwright)
+    # producer = KafkaProducer(bootstrap_servers=["localhost:9092"], max_block_ms=5000)
+    producer = "None"
+    # async with async_playwright() as playwright:
+    #     await run(playwright, producer)
+    await run(producer)
 
 
 if __name__ == '__main__':
---
+++ spark_consumer.py
@@ -0,0 +1,110 @@
+import logging
+from cassandra.cluster import Cluster
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+
+def create_keyspace(session):
+    session.execute(
+        """
+    CREATE KEYSPACE IF NOT EXISTS property_streams
+    WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};
+    """
+    )
+    print("Keyspace created cuccessfully!!!")
+
+
+def create_table(session):
+    session.execute(
+        """
+        CREATE TABLE IF NOT EXISTS property_streams.properties(
+            price text,
+            title text,
+            link text,
+            prictures list<text>,
+            floor_plan text,
+            address text,
+            bedrooms text,
+            bathrooms text,
+            receptions text,
+            epc_rating text,
+            tenure text,
+            time_remaining_on_lease text,
+            service_charge text,
+            council_tax_band text,
+            ground_rent text,
+            PRIMARY KEY (link)
+        );      
+        """)
+
+    print("Table created successfully!!!")
+
+
+def create_cassandra_session():
+    session = Cluster(["localhost"]).connect()
+    if session is not None:
+        create_keyspace(session)
+        create_table(session)
+    return session
+
+
+def insert_data(session, **kwargs):
+    print("Inserting data...")
+    session.execute(
+        """
+        INSERT INTO property_streams.properties(price, title, link
+        pictures, floor_plan, bedrooms, bathrooms, receptions, epc_ratings, tenure,
+        time_remaining_on_lease, service_charge, council_tax_band, ground_rent)
+        VALUES($s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,)
+        """, kwargs.values())
+
+    print("Data inserted successfully!!!")
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    spark = SparkSession.builder.appName("RealEstateConsumer")\
+             .config("spark.cassandra.connection.host", "localhost")\
+             .config("spark.jar.packages", "com.datastax.spark:spark-cassandra-connector_2.12:3.4.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0").getOrCreate()
+
+    kafka_broker = "localhost:9092"
+    topic_name = "properties"
+    kafka_df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_broker)\
+        .option("subcribe", topic_name)\
+        .option("startingOffsets", "earliest")\
+        .load()
+
+    schema = StructType([
+        StructField("price", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("link", StringType(), True),
+        StructField("pictures", ArrayType(StringType()), True),
+        StructField("floor_plan", StringType(), True),
+        StructField("bedrooms", StringType(), True),
+        StructField("bathrooms", StringType(), True),
+        StructField("receptions", StringType(), True),
+        StructField("EPC Rating", StringType(), True),
+        StructField("tenure", StringType(), True),
+        StructField("time_remaining_on_lease", StringType(), True),
+        StructField("service_charge", StringType(), True),
+        StructField("council_tax_band", StringType(), True),
+        StructField("ground_rent", StringType(), True),
+    ])
+
+    kafka_df = kafka_df.selectExpr("CAST(value AS STRING) as value")\
+        .select(from_json(col("value"), schema)).alias("data")\
+        .select("data.*")
+
+    cassandra_query = kafka_df.writeStream\
+        .foreachBatch(
+            lambda batch_df, batch_id: batch_df.foreach(
+                lambda row: insert_data(create_cassandra_session(), **row.asDict())))\
+        .start().awaitTermination()
+
+if __name__ == "__main__":
+    main()
